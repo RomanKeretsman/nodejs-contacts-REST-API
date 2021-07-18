2bf0c2c7020e7cefea6e76d79588844ba8da9773
Roman Keretsman <rkeretsman@gmail.com>
2021-07-18 21:21:36 +0300

    end-hw-mongodb

---
+++ helpers/asyncWrapper.js
@@ -0,0 +1,11 @@
+// Мидлвар для универсальной замены try catch в контроллерах
+
+const asyncWrapper = (controller) => {
+  return (req, res, next) => {
+    controller(req, res).catch(next)
+  }
+}
+
+module.exports = {
+  asyncWrapper,
+}
